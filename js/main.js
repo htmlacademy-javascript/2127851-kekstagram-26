62b233f7e4dc5e532c173fb3afac220d19918fec
1G000 <gorbatovskiy.igor@gmail.com>
2022-09-18 14:35:09 +0300

    another try

--- js/main.js
+++ js/main.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 import { generateDescriptionsObjects } from "./util.js";
 generateDescriptionsObjects();
-=======
-import {generatePhotoDescription} from './data.js';
-generatePhotoDescription();
->>>>>>> 401146cacd248c1b58fd6ff14d8ad0f4015b38ac
--- js/util.js
+++ js/util.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { MESSAGES, NAMES, DESCRIPTIONS } from "./data.js";
 
 const generateMessage = function () {
@@ -50,33 +49,3 @@ const generateDescriptionsObjects = function () {
 };
 
 export { generateDescriptionsObjects };
-=======
-import {COMMENTS_TEXT} from './data.js';
-function getRandomNumber (min, max) {
-  if (min >= 0 && min < max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
-}
-
-function checkStringLength (myString, maxStringLength) {
-  if (myString.length <= maxStringLength) {
-    return true;
-  }
-  return false;
-}
-checkStringLength(78, 589);
-
-function getMessage () {
-  let message = '';
-  for (let i = 1; i <= getRandomNumber(1, 3); i++) {
-    message += `${COMMENTS_TEXT[getRandomNumber(0, COMMENTS_TEXT.length-1)]  } `;
-  }
-  return message.trim();
-}
-getMessage ();
-
-function getRandomCommentFromArray (comments)  {
-  return comments[getRandomNumber(0, comments.length - 1)];
-}
-export {getRandomNumber, getMessage, getRandomCommentFromArray};
->>>>>>> 401146cacd248c1b58fd6ff14d8ad0f4015b38ac
